2f495c90ba733101e7b46d69beb81d6a81df0c61
AmaarElgml <amaarreda31@gmail.com>
2022-04-07 20:35:51 +0200

    added settings

---
+++ lib/view/prayer_timings/index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrayerTimings extends StatelessWidget {
+  const PrayerTimings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
