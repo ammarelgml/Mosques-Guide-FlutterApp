f859d0a924da5d558d7f0987dea01a5fbb4641dc
Karim Bouzid <41644989+Karim-bz@users.noreply.github.com>
2022-04-16 04:17:28 +0100

    Create Reusable Button

--- lib/view/settings/index.dart
+++ lib/view/settings/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mosques_guide_app/view/settings/widgets/button.dart';
 import 'package:mosques_guide_app/view/settings/widgets/header_section.dart';
 
 class Settings extends StatelessWidget {
@@ -8,9 +9,29 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
         SettingsHeader(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 22.0),
+          child: Column(
+            children: <Widget>[
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "معلومات التطبيق",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Button(onPressed: onPressed, title: "اللغة"),
+            ],
+          ),
+        )
       ],
     ));
+  }
+
+  onPressed() {
+    print("ClickMe");
   }
 }
